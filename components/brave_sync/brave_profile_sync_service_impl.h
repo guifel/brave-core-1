e47ae552443f31b2175b529445f1f34d424de230
mkarolin <max@brave.com>
2020-02-12 13:44:47 -0500

    SyncService::DisableReason is now an EnumSet.
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/a30f44ca534879c76ab4c7a00b3560ad71c29181
    
    commit a30f44ca534879c76ab4c7a00b3560ad71c29181
    Author: Sergey Talantov <sergey.talantov@gmail.com>
    Date:   Tue Dec 17 13:38:00 2019 +0000
    
        Migrate SyncService::DisableReason to EnumSet
    
        SyncService::DisableReason was implemented as simple bitmask.
        For this purpose EnumSet is more appropriate.
    
        Bug: 864556

--- components/brave_sync/brave_profile_sync_service_impl.h
+++ components/brave_sync/brave_profile_sync_service_impl.h
@@ -109,7 +109,7 @@ class BraveProfileSyncServiceImpl
       std::unique_ptr<brave_sync::RecordsList> records) override;
 
   // syncer::SyncService implementation
-  int GetDisableReasons() const override;
+  syncer::SyncService::DisableReasonSet GetDisableReasons() const override;
   CoreAccountInfo GetAuthenticatedAccountInfo() const override;
   bool IsAuthenticatedAccountPrimary() const override;
 
